2fe588ac9755ac0bdd4b4fec2c03f048e7e0752b
rafaelandradegs <rafaelandradegs@gmail.com>
2021-05-24 16:45:51 -0300

    Tipos de Dados

---
+++ tipos_de_dados/exercicios.js
@@ -0,0 +1,20 @@
+// Declare uma variavel contendo uma string
+var educacao = 'Bom dia';
+
+// Declare uma variavel contendo um numero dentro de uma string
+var dia = '24';
+
+// Declare uma variavel com a sua idade 
+var idade = 33;
+
+// Declare duas variaveis, uma com o seu nome e a outra com seu sobrenome e some as mesmas
+var nome = 'Rafael',
+    sobrenome = 'Andrade';
+
+console.log(`${nome} ${sobrenome}`);
+
+// Coloque a seguinte frase em uma variavel: it's time
+var frase = "it's time"
+
+// Verifique o tipo da variavel que contem o seu nome
+console.log(typeof nome);
---
+++ tipos_de_dados/script.js
@@ -0,0 +1,28 @@
+var idade = 33;
+var simbolo = Symbol();
+console.log(typeof simbolo)
+
+var nome = 'Rafael';
+var sobrenome = 'Andrade';
+var nomeCompleto = nome + ' ' + sobrenome
+
+console.log(nomeCompleto);
+
+var gols = 1000;
+var frase = 'Romario fez ' + gols + 'gols';
+
+console.log(typeof frase);
+
+var ano = 2018;
+var mes = 8;
+
+console.log(ano + mes);
+
+var frase1 = 'Esse e o melhor jogo'
+
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romario fez mais de ${gols * 2} gols`
+
+console.log(frase2);

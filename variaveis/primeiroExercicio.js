769eae38a7f98aab73612fde70d0f688cdfce271
rafaelandradegs <rafaelandradegs@gmail.com>
2021-05-24 15:08:13 -0300

    Variaveis

---
+++ variaveis/primeiroExercicio.js
@@ -0,0 +1,23 @@
+// Declarar uma variavel com o seu nome
+var name = 'Rafael';
+
+// Declarar uma varial com a sua idade
+var idade = 33;
+
+// Declarar uma varial com a sua comida favorita e nao atribuir valor
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = 'KFC';
+
+//Declarar 5 variaveis diferentes sem valores
+
+var jogo = 'Warzone',
+    carro = 'Hilux',
+    esposa,
+    filho,
+    filha;
+
+
+
+console.log(comidaFavorita);
---
+++ variaveis/script.js
@@ -0,0 +1,33 @@
+var nome = 'Joao';
+var idade = 28;
+var possuiFaculdade = idade > 24 ? 'Possui faculdade' : 'Nao possui faculdade'
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco
+
+console.log(totalPreco);
+console.log('Hello Word ola mundo');
+
+var sobrenome = 'Andrade',
+    nascido = ' Ji-Parana';
+
+console.log(sobrenome, nascido);
+
+var semDefinir;
+console.log(semDefinir);
+
+///console.log(aindaNaoDefine); 
+
+var comida;
+console.log(comida);
+comida = 'pizza';
+console.log(comida);
+
+// const time = 'Vasco';
+//time = 'Flamengo';
+
+//console.log(time);
+

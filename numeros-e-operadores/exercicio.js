5e197d4b703831d735926e1ac9c9669d73d3442c
rafaelandradegs <rafaelandradegs@gmail.com>
2021-05-24 21:11:16 -0300

    Numeros e Operadores

---
+++ numeros-e-operadores/exercicio.js
@@ -0,0 +1,27 @@
+// Qual o resultado da seguinte expressao? = 35
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+
+// Crie duas expressoes que retornem NaN
+var expressao1 = 1 * 'dois';
+var expressao2 = 'teste' - 6;
+
+console.log(expressao1, expressao2);
+
+// Somar a String '200' com o numero 50 e retornar 250
+var soma = +'200' + 50
+
+console.log(soma);
+
+// Incremente o Numero 5 e retorne o seu valor incrementado
+var incremento = 5;
+incremento++
+
+console.log(incremento);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+
+console.log(peso);
---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,29 @@
+var total1 = 10 + 5 + 10;
+var divisao = 200 / 5;
+var modulo = 10 % 2
+
+console.log(modulo);
+
+var testeNaN = 'isso e 100' / 2
+
+console.log(isNaN(testeNaN));
+
+var soma1 = 10 + 10 + 20 + 30 * 4 / 2 + 10;
+console.log(soma1);
+
+// Operadores Aritimeticos Unario de incremento
+var x = 5;
+console.log(x++);
+console.log(x);
+
+// Operadores Aritimeticos Unario de decremento
+var y = 5;
+console.log(x--);
+console.log(x);
+
+var idade = '28';
+var somaIdade = 5;
+
+console.log(+idade + somaIdade);
+
+

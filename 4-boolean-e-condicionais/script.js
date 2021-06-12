c6aa6b5c0be61bd17d9997d35dde6243a19e9505
rafaelandradegs <rafaelandradegs@gmail.com>
2021-06-12 17:36:52 -0300

    Organizando pastas

---
+++ 4-boolean-e-condicionais/script.js
@@ -0,0 +1,23 @@
+var possuiGraduacao = false;
+var possuiDoutorado = true;
+
+if (possuiGraduacao) {
+    console.log('E Graduado');
+} else if (possuiDoutorado) {
+    console.log('Possui Doutorado');
+} else {
+    console.log('Vai Estudadar preguicoso.');
+}
+
+var nome = '10kg' / 10;
+
+if (nome) {
+    console.log(nome);
+} else {
+    console.log('Nome nao existe');
+}
+
+if (!possuiGraduacao) {
+    console.log('Parabens');
+}
+

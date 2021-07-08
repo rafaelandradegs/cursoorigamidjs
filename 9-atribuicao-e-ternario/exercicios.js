fd8b31df4bd973aa10270d6c46db3bbe9cf4e599
rafaelandradegs <rafaelandradegs@gmail.com>
2021-07-08 14:27:04 -0300

    atribuicao e ternario

---
+++ 9-atribuicao-e-ternario/exercicios.js
@@ -0,0 +1,18 @@
+// Some 500 ao valor de scroll abaixo
+// Atribuindo o novo valor a scroll
+var scroll = 1000
+scroll += 500
+
+console.log(scroll);
+
+
+// Atribua true para a variavel darCredito
+// caso o cliente possua carro e casa.
+// E false caso o contrario 
+var possuiCarro = true
+var possuiCasa = true
+
+
+var darCredito = (possuiCarro && possuiCasa == true) ? true : false
+
+console.log(darCredito);
---
+++ 9-atribuicao-e-ternario/script.js
@@ -0,0 +1,28 @@
+// Operadores de atribuicao
+var numero = 20;
+var numero2 = 10
+
+numero -= numero2
+// +=
+// -=
+// *=
+// /=
+// **=
+console.log(numero);
+
+var idade = 20
+var naoPossuiDiabetes = true
+var podeBeber = (idade >= 18 && naoPossuiDiabetes) ? 'Pode beber' : 'nao pode beber'
+
+console.log(podeBeber)
+
+var possuiFaculdade = true
+
+if (possuiFaculdade) console.log('Possui Faculdade');
+else console.log('nao possui facauldade');
+
+var possuiFaculdade = false
+
+if (possuiFaculdade)
+    console.log('possui faculdade');
+else console.log('nao possui faculdade');

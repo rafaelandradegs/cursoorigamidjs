38b5afaa82b40c17667a1440c8a6bc7b4b8d98d6
Rafael A Gonçalves <rafaelandradegs@gmail.com>
2021-09-15 17:20:22 -0300

    Accordion List

--- 19-navegacao-por-tab/script.js
+++ 19-navegacao-por-tab/script.js
@@ -1,19 +1,19 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+const tabMenu = document.querySelectorAll('.js-tabmenu li')
+const tabContent = document.querySelectorAll('.js-tabcontent section')
 
 if (tabMenu.length && tabContent.length) {
   tabContent[0].classList.add('ativo')
 
   function activeTab(index) {
-    tabContent.forEach((content) => {
-      content.classList.remove('ativo');
+    tabContent.forEach(item => {
+      item.classList.remove('ativo')
     })
-    tabContent[index].classList.add('ativo');
+    tabContent[index].classList.add('ativo')
   }
 
-  tabMenu.forEach((itemMenu, index) => {
-    itemMenu.addEventListener('click', () => {
-      activeTab(index);
-    });
-  });
+  tabMenu.forEach((image, index) => {
+    image.addEventListener('click', () => {
+      activeTab(index)
+    })
+  })
 }

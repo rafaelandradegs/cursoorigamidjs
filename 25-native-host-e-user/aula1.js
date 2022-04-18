21ff3212d5c8722af883eb711bdfff2b5c9f40b6
Rafael Andrade Goncalves <rafgon@cpqd.com.br>
2022-04-18 15:48:29 -0300

    native-host-e-user

---
+++ 25-native-host-e-user/aula1.js
@@ -0,0 +1,5 @@
+if (typeof Array.from !== 'undefined') {
+  console.log('oi')
+} else {
+  console.log('tente novamente')
+}
---
+++ 25-native-host-e-user/script.js
@@ -0,0 +1,9 @@
+// Liste 5 objetos nativos
+
+// Namber, Undefined, Null, Bollean, String
+
+// Liste  objetos do browser
+// Window, Navigator, Location, Histoy, Screen
+
+// Liste 2 métodos, propriedades ou objetos
+// presentes no chrome que nao exstem no firefox

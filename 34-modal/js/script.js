54aea3f5f674a300e65a4e79e8779575a8ffeb6d
Rafael Andrade Goncalves <rafgon@cpqd.com.br>
2022-05-16 18:51:36 -0300

    modal

--- 34-modal/js/script.js
+++ 34-modal/js/script.js
@@ -2,7 +2,9 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimationScroll from "./modules/scroll-animation.js";
 import initAccordion from "./modules/init-accordion.js";
 import initTabNav from "./modules/init-tab-nav.js";
+import initModal from "./modules/modal.js";
 
+initModal();
 initScrollSuave();
 initAnimationScroll();
 initAccordion();

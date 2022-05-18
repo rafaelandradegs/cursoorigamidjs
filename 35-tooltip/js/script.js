09060fb20fb9a6f5bd92848c79fd6f2e9c54deb6
Rafael Andrade Goncalves <rafgon@cpqd.com.br>
2022-05-18 11:12:05 -0300

    tooltip

---
+++ 35-tooltip/js/script.js
@@ -0,0 +1,13 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimationScroll from "./modules/scroll-animation.js";
+import initAccordion from "./modules/init-accordion.js";
+import initTabNav from "./modules/init-tab-nav.js";
+import initModal from "./modules/modal.js";
+import tooltip from "./modules/tooltip.js";
+
+tooltip();
+initModal();
+initScrollSuave();
+initAnimationScroll();
+initAccordion();
+initTabNav();

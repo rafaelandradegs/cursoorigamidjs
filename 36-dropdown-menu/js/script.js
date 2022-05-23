c2edfb7c92f097ac67958cd68bc38e9662a25d70
Rafael Andrade Goncalves <rafgon@cpqd.com.br>
2022-05-23 01:20:07 -0300

    dropdown-menu-e-clickoutside

---
+++ 36-dropdown-menu/js/script.js
@@ -0,0 +1,15 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimationScroll from "./modules/scroll-animation.js";
+import initAccordion from "./modules/init-accordion.js";
+import initTabNav from "./modules/init-tab-nav.js";
+import initModal from "./modules/modal.js";
+import tooltip from "./modules/tooltip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+
+initDropdownMenu();
+tooltip();
+initModal();
+initScrollSuave();
+initAnimationScroll();
+initAccordion();
+initTabNav();
